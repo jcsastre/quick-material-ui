a0293d89af2bf80a990188ae72280785a59ce138
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-04-29 16:47:54 +0200

    Main content and footer.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import TopMenu from '../components/TopMenu';
 import SideMenu from '../components/SideMenu';
+import MainContent from '../components/MainContent';
+import Footer from '../components/Footer';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,6 +20,8 @@ const Home = () => {
     <div className={classes.root}>
       <TopMenu />
       <SideMenu />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
